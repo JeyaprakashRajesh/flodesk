76bddf474daa008787a8095af9f124c9be836cd4
JeyaprakashRajesh <r.jeya2005@gmail.com>
2025-07-09 10:20:31 +0530

    removeed all

--- client/src/renderer/src/components/ProjectScreen/Tools/Clock/Clock.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './Clock.css'
-
-export default function Clock() {
-  return <div className="tools-clock-container"></div>
-}
--- client/src/renderer/src/utilities/Tools.js
+++
@@ -1,9 +0,0 @@
-export const clockTools = [
-  {
-    name: 'clock',
-    img: '',
-    props  : {
-      position: { x: 0, y: 0 },
-    }
-  }
-]
